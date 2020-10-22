1db38dacfceb3657a9d9804459298b42607ce85e
Fernando Acosta <contato@fernandoacosta.net>
2020-10-22 19:12:52 -0300

    configurar deploy para WP.org

---
+++ build/pre-zip.php
@@ -0,0 +1,19 @@
+<?php
+echo "Publish to WP.org? (Y/n) ";
+if ( 'Y' === trim( strtoupper( fgets( STDIN ) ) ) ) {
+  echo `svn co -q https://plugins.svn.wordpress.org/wc-correios-easy-tracking-code svn`;
+  echo `rm -R svn/trunk`;
+  echo `mkdir svn/trunk`;
+  echo `mkdir svn/tags/$version`;
+  echo `rsync -r $plugin_slug/* svn/trunk/`;
+  echo `rsync -r $plugin_slug/* svn/tags/$version`;
+  echo `svn stat svn/ | grep '^\?' | awk '{print $2}' | xargs -I x svn add x@`;
+  echo `svn stat svn/ | grep '^\!' | awk '{print $2}' | xargs -I x svn rm --force x@`;
+  echo `svn stat svn/`;
+
+  echo "Commit to WP.org? (Y/n)? ";
+  if ( 'Y' == trim( fgets( STDIN ) ) ) {
+    echo `svn ci svn/ -m "Deploy version $version"
+    `;
+  }
+}
