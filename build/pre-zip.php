0d1ae9e9e68aef17298bc805098fe588b3895e11
Fernando Acosta <contato@fernandoacosta.net>
2020-10-22 19:31:26 -0300

    incluindo senha

--- build/pre-zip.php
+++ build/pre-zip.php
@@ -1,6 +1,15 @@
 <?php
 echo "Publish to WP.org? (Y/n) ";
 if ( 'Y' === trim( strtoupper( fgets( STDIN ) ) ) ) {
+  if ( file_exists( __DIR__ . '/wp-org.php' ) ) {
+    include __DIR__ . '/wp-org.php';
+  }
+
+  if ( ! isset( $username, $password ) ) {
+    echo "Ignorando Deploy WP.org porque usuário e senha não foram definidos. \n";
+    return;
+  }
+
   echo `svn co -q https://plugins.svn.wordpress.org/wc-correios-easy-tracking-code svn`;
   echo `rm -R svn/trunk`;
   echo `mkdir svn/trunk`;
@@ -12,8 +21,8 @@ if ( 'Y' === trim( strtoupper( fgets( STDIN ) ) ) ) {
   echo `svn stat svn/`;
 
   echo "Commit to WP.org? (Y/n)? ";
-  if ( 'Y' == trim( fgets( STDIN ) ) ) {
-    echo `svn ci svn/ -m "Deploy version $version"
+  if ( 'Y' === trim( strtoupper( fgets( STDIN ) ) ) ) {
+    echo `svn ci svn/ -m "Deploy version $version" --username $username --password $password
     `;
   }
 }
